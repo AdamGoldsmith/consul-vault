6914594bff78be5b2583f969663039db424eabb1
Goldsmith, Adam <adam.goldsmith75@gmail.com>
2018-06-01 08:58:06 +0100

    Add no-exec option to deploy.sh to display what ansible options would be used

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ HELP="Deploy, manage or destroy HA consul + vault environment.
 ${USAGE}\n
   -s = Service name [consul|vault] (Optional - both if omitted)
   -t = Tags [tag1,tag2,tag3] (Optional - all necessary tags for full deployment if omitted)
+  -n = No execution, just display the ansible command that would run
   -h = Show usage
 
 Examples\n
@@ -29,11 +30,12 @@ Examples\n
   Equivalent to running \"ansible-playbook -i inventory site.yml --tags 'remove'\"
 "
 
-while getopts s:t:h name
+while getopts s:t:nh name
 do
   case ${name} in
   s)    service=${OPTARG};;
   t)	tags=${OPTARG:-};;
+  n)	norun=1;;
   h)    printf "\n${HELP}\n"; exit;;
   \?)   printf "\nBad usage!\n${USAGE}\n"; exit 1;;
   esac
@@ -42,5 +44,5 @@ done 2>/dev/null
 cmd="ansible-playbook -i inventory ${service:-site}.yml --tags '${tags:-epel,install,init,unseal,configure,approle,sshkeysign}'"
 
 printf "\n${cmd}\n"
-eval ${cmd}
+[[ -z ${norun} ]] && eval ${cmd} || printf "\n"
 
